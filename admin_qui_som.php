c491c357d4991b9bc568b356e07205c0cbc030b1
omainou <116905342+omainou@users.noreply.github.com>
2022-12-07 23:40:12 +0100

    Administrador "qui som"

--- admin_qui_som.php
+++ admin_qui_som.php
@@ -22,13 +22,46 @@
         <h3>Administrar pàgina "Qui som?"</h3>
         <p>Aquí es mostra el text que es visualitza a la pàgina "Qui som?".</p>
 
+        <?php
+          if (isset($_POST["submit"])) {
+            $quisom = $_POST["quisom"];
+            $sessions = $_SESSION['id_usuari_sessio'];
+
+            $sql_update = "UPDATE qui_som SET text=?, id_persona=? WHERE id=1";
+            $resultat_update = $connexio_PDO->prepare($sql_update);
+
+            $resultat_update->execute([ $quisom , $sessions ]);
+
+            echo "<div class='alert alert-success' role='alert'>Editat correctament.</div>";
+          }
+        ?>
+
         <form action="admin_qui_som.php" method="post" class="row g-3">
           <div class="col-12">
             <label for="quisom" class="form-label">
               Descripció
               <span class="text-muted" id="max_paraules">[Màxim de lletres: 650]</span>
             </label>
-            <textarea name="quisom" id="quisom" rows="9" class="form-control" maxlength="650" required>...</textarea>
+
+            <?php
+            $sql_qui_som = "SELECT * FROM qui_som";
+            $resultat_qui_som = $connexio->query($sql_qui_som);
+
+            if ($resultat_qui_som->num_rows > 0) {
+              while ($row_qui_som = $resultat_qui_som->fetch_assoc()) {
+                ?>
+                <textarea name="quisom" rows="8" class="form-control" maxlength="650" required><?php echo $row_qui_som["text"]; ?></textarea>
+                <?php
+
+                $sql_usuari = "SELECT * FROM usuari WHERE id = " . $row_qui_som["id_persona"];
+                $resultat_usuari = $connexio->query($sql_usuari);
+
+                while ($row_usuari = $resultat_usuari->fetch_assoc()) {
+                  echo "<p class='text-muted'>Últim usuari que va editar la pàgina: " . $row_usuari["nom"] . "</p>";
+                }
+              }
+            }
+            ?>
           </div>
 
           <div class="col-12">
